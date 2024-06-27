f518369d79ecd3f4aea55b795cee7610710ac22d
nilesh9758 <nileshchaubey9702@gmail.com>
2024-06-27 16:34:24 +0530

    first commit

---
+++ src/components/featuredProperties/FeaturedPropertise.jsx
@@ -0,0 +1,64 @@
+import "./featuredPropertise.css";
+
+export const FeaturedPropertise = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/123801934.webp?k=27073a18101dd5a4eefc76251f7d476be72e19ed03e98819f2d94667dd60f31a&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Ardsdue uijsaj okassa</span>
+        <span className="fpCity">Hyderabad</span>
+        <span className="fpPrice">Strating from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/123801934.webp?k=27073a18101dd5a4eefc76251f7d476be72e19ed03e98819f2d94667dd60f31a&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Maharaja Hotels</span>
+        <span className="fpCity">Jaipur</span>
+        <span className="fpPrice">Strating from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/123801934.webp?k=27073a18101dd5a4eefc76251f7d476be72e19ed03e98819f2d94667dd60f31a&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Ardsdue uijsaj okassa</span>
+        <span className="fpCity">Delhi</span>
+        <span className="fpPrice">Strating from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/123801934.webp?k=27073a18101dd5a4eefc76251f7d476be72e19ed03e98819f2d94667dd60f31a&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Ardsdue uijsaj okassa</span>
+        <span className="fpCity">Bengaluru</span>
+        <span className="fpPrice">Strating from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
